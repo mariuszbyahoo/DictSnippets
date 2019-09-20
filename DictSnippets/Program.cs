f516a35f21e6f20c178838f4fdf27a2d712bb26a
Mariusz Budzisz <mariusz.budzisz@yahoo.com>
2019-09-20 23:51:04 +0200

    Dictionary Initializers

--- DictSnippets/Program.cs
+++ DictSnippets/Program.cs
@@ -10,10 +10,11 @@ namespace DictSnippets
             Country norway = new Country("Norway", "NOR", "Europe", 5_282_223);
             Country finland = new Country("Finland", "FIN", "Europe", 5_511_303);
 
-            var countries = new Dictionary<string, Country>();
-
-            countries.Add(norway.getCode(), norway);
-            countries.Add(finland.getCode(), finland);
+            var countries = new Dictionary<string, Country>
+            {
+                { norway.getCode(), norway },
+                { finland.getCode(), finland }
+            };
 
             foreach (var country in countries.Values)
             {
